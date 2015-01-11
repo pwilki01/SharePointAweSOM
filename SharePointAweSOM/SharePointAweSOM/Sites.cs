6e1825cd7ee706a23b3803a4c5217a02bdf73f1e
pwilki01 <pwilki01@gmail.com>
2015-01-10 21:25:46 -0500

    Initial Check in of Site class

---
+++ SharePointAweSOM/SharePointAweSOM/Sites.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+
+using Microsoft.SharePoint.Client;
+
+namespace SharePointAweSOM
+{
+    public class Sites : IDisposable
+    {
+        private readonly ClientContext clientContext;
+
+        public Sites(string siteUrl, ICredentials credentials)
+        {
+            this.clientContext = new ClientContext(siteUrl) { Credentials = credentials };
+        }
+
+        public Site GetSite()
+        {
+            var site = this.clientContext.Site;
+            this.clientContext.Load(site);
+            this.clientContext.ExecuteQuery();
+            return site;
+        }
+
+        public void Dispose()
+        {
+            if (this.clientContext != null)
+            {
+                this.clientContext.Dispose();
+            }
+        }
+    }
+}
